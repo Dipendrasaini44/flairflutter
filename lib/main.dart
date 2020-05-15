1bc4715535079affef5593d72bcb8bbca174dc3e
dipendrasaini <dipendrasaini44@gmail.com>
2020-05-15 09:40:55 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     debugShowCheckedModeBanner: false,
+     home: Myhome(),
+    );
+  }
+}
+
+class Myhome extends StatefulWidget {
+  @override
+  _MyhomeState createState() => _MyhomeState();
+}
+
+class _MyhomeState extends State<Myhome> 
+{
+  bool istrue =true;
+
+  @override
+  Widget build(BuildContext context) 
+  {
+    return Scaffold(
+
+      body:Center(
+        child:
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+        children :<Widget>[
+          AnimatedCrossFade
+        (
+          firstChild: FlutterLogo
+          (
+          style: FlutterLogoStyle.horizontal,
+          size: 300.0,
+          )
+         , secondChild: FlutterLogo(
+           style: FlutterLogoStyle.stacked,
+           size: 300.0,
+         ),
+         crossFadeState:istrue ? CrossFadeState.showFirst : CrossFadeState.showSecond, 
+         duration: Duration(milliseconds: 1000),
+        ),
+
+        SizedBox(
+          height:20
+        ),
+        Container(
+          height: 40,
+          width: 40,
+          color: Colors.greenAccent,
+          child: IconButton(icon: Icon(Icons.play_arrow,color: Colors.blueAccent,),
+           onPressed: (){
+             setState(() {
+               if(istrue ==true)
+               {
+                 istrue=false;
+                 print("hi");
+               }
+               else{
+                 istrue =true;
+                 print("hello");
+               }
+             });
+           }),
+        )
+        ], 
+      )),
+
+    );
+      
+    
+  }
+}
